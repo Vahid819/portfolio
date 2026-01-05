a67a4d886025ba2d50fd403b09bc9506c1fe737b
vahid819 <vahidmomin1819@gmail.com>
2026-01-05 20:00:57 +0530

    create zod schema file and done database file an model

---
+++ src/lib/db.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/portfolio'
+
+if (!MONGODB_URL) {
+    throw new Error('Please define the MONGODB_URL environment variable inside .env');
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect(){
+    if (cached.conn) {
+        return cached.conn;
+    }
+
+    if (!cached.promise) {
+        const opts = {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        };
+    }
+    cached.promise = mongoose.connect(MONGODB_URL, opts).then((mongoose) => {
+        return mongoose;
+    });
+    cached.conn = await cached.promise;
+    return cached.conn;
+}
---
+++ src/model/User.js
@@ -0,0 +1,65 @@
+import { Schema } from "mongoose";
+import mongoose from "mongoose";
+import { required } from "zod/v4/core/util.cjs";
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        match: [
+            /^(?=.{3,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
+            "Username must be 3–20 characters and can contain letters, numbers, _ or ."
+        ]
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [
+            /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+            "Please enter a valid email address"
+        ]
+    },
+    verificationCode: {
+        type: Number,
+        required: true,
+    },
+
+    verifiedexpiredate:{
+        type: Date,
+        required: true,
+    },
+
+    verifiedemail: {
+        type: Boolean,
+        default: false
+    },
+    password: {
+        trype: String,
+        required: true,
+    },
+    login: {
+        type: Boolean,
+        default: false
+    },
+    logindate: {
+        type: Date,
+        default: Date.now()
+    },
+})
+
+
+const UserModel = (mongoose.model.User) || mongoose.model("User",UserSchema);
+
+export default UserModel;
