0e3b385fbf1e38d0d861e93d99929428029148ce
vahid819 <vahidmomin1819@gmail.com>
2026-01-09 21:05:44 +0530

    changes in db connection file

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,30 +2,31 @@ import mongoose from 'mongoose';
 
 const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/portfolio'
 
-if (!MONGODB_URL) {
-    throw new Error('Please define the MONGODB_URL environment variable inside .env');
-}
 
-let cached = global.mongoose;
+const connection = {}
 
-if (!cached) {
-    cached = global.mongoose = { conn: null, promise: null };
-}
-
-async function dbConnect(){
-    if (cached.conn) {
-        return cached.conn;
+async function dbConnection() {
+    if(connection.isConnected){
+        console.log("Database alredy connected");
+        return
     }
 
-    if (!cached.promise) {
-        const opts = {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        };
+    try {
+        const db = await mongoose.connect(MONGODB_URL);
+        
+        connection.isConnected = db.connections[0].readyState;
+        
+        console.log("db connection successfully");
+        console.log(db);
+        
+        
+        
+    } catch (error) {
+
+        console.log("Databse connection faild", error)
+        process.exit(0);
+        
     }
-    cached.promise = mongoose.connect(MONGODB_URL, opts).then((mongoose) => {
-        return mongoose;
-    });
-    cached.conn = await cached.promise;
-    return cached.conn;
 }
+
+export default dbConnection; 
